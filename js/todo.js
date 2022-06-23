1b721c51d39d1ce2231446eb2a2d3f22b90c2fef
hyukjinKimm <hyukjin.kim.dev@gmail.com>
2022-06-23 21:09:26 +0900

    feat: Edit todo.js to include Deleting To Dos

--- js/todo.js
+++ js/todo.js
@@ -10,16 +10,20 @@ function saveToDos(){
 }
 function deleteToDo(e){
   const li = e.target.parentElement;
-
+  toDos = toDos.filter((toDo) => {
+    return toDo.id !== parseInt(li.id);
+  });
+  saveToDos();
   li.remove();
 }
 function paintToDo(newTodo){
   const li = document.createElement('li');
+  li.id = newTodo.id;
   const span = document.createElement('span');
   const button = document.createElement('button');
   button.innerText = 'X';
   button.addEventListener('click', deleteToDo)
-  span.innerText = newTodo;
+  span.innerText = newTodo.text;
   li.appendChild(span);
   li.appendChild(button);
   toDoList.appendChild(li);
@@ -28,8 +32,13 @@ function handleToDoSubmit(e){
   e.preventDefault();
   const newTodo = toDoInput.value;
   toDoInput.value = '';
-  toDos.push(newTodo);
-  paintToDo(newTodo);
+  const newToDoObj = {
+    text: newTodo,
+    id: Date.now(),
+
+  }
+  toDos.push(newToDoObj);
+  paintToDo(newToDoObj);
   saveToDos();
 }
 toDoForm.addEventListener('submit', handleToDoSubmit);
