b7b651dfa51f60e5e0147382ab7d05e033eb4ca5
hyukjinKimm <hyukjin.kim.dev@gmail.com>
2022-06-23 20:56:52 +0900

    feat: Edit todo.js to include Loading To Dos

--- js/todo.js
+++ js/todo.js
@@ -1,9 +1,16 @@
 const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
+const TODOS_KEY = "todos";
 
+let toDos = [];
+
+function saveToDos(){
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+}
 function deleteToDo(e){
   const li = e.target.parentElement;
+
   li.remove();
 }
 function paintToDo(newTodo){
@@ -21,6 +28,18 @@ function handleToDoSubmit(e){
   e.preventDefault();
   const newTodo = toDoInput.value;
   toDoInput.value = '';
+  toDos.push(newTodo);
   paintToDo(newTodo);
+  saveToDos();
 }
 toDoForm.addEventListener('submit', handleToDoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if(savedToDos){
+  const parsedToDos = JSON.parse(savedToDos);
+  toDos = parsedToDos;
+  parsedToDos.forEach(paintToDo)
+} else {
+
+}
